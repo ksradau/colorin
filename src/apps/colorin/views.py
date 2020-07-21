3e2dd0c79d8fe025f397e9d06f8e2e3084d25309
ksradau <sushei.ekaterina@gmail.com>
2020-07-21 23:56:30 +0300

    Colorin view simplified

--- src/apps/colorin/views.py
+++ src/apps/colorin/views.py
@@ -6,19 +6,3 @@ from apps.colorin.models import ColorinPage
 class IndexView(TemplateView):
     template_name = "colorin/index.html"
 
-    def get_context_data(self, **kwargs):
-        parent_ctx = super().get_context_data(**kwargs)
-
-        info = ColorinPage.objects.first()
-        if info is not None:
-            ctx = {
-                "title": info.title,
-                "description": info.description,
-                "h1": info.h1,
-                "university": info.university,
-                "text": info.text,
-            }
-
-            ctx.update(parent_ctx)
-
-            return ctx
