09cad7e07b271032e9a95307ada7e852eea4940d
ksradau <sushei.ekaterina@gmail.com>
2020-08-13 20:44:29 +0300

    Forms for Colorin Index with HiddenInput

--- src/apps/colorin/forms.py
+++ src/apps/colorin/forms.py
@@ -1,5 +1,25 @@
 from django import forms
+from django.forms import HiddenInput, ModelForm
+from apps.colorin.models import InstagramPhoto, InstagramProfile, UploadedPhoto
 
 
 class FileFieldForm(forms.Form):
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+
+class UpdateInfoForm(ModelForm):
+    class Meta:
+        model = InstagramProfile
+        widgets = {'any_field': HiddenInput(), }
+
+
+class UpdateIGPhotoForm(ModelForm):
+    class Meta:
+        model = InstagramPhoto
+        widgets = {'any_field': HiddenInput(), }
+
+
+class DownLoadForm(ModelForm):
+    class Meta:
+        model = UploadedPhoto
+        widgets = {'any_field': HiddenInput(), }
