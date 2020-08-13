6a7a4afa23249ac2d930b21f9dd1ef8dc2aa3fd7
ksradau <sushei.ekaterina@gmail.com>
2020-08-13 21:02:12 +0300

    Views for multiply forms started

--- src/apps/colorin/views.py
+++ src/apps/colorin/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 
 from django.views.generic.edit import FormView
-from .forms import FileFieldForm
+from .forms import FileFieldForm, UpdateInfoForm, UpdateIGPhotoForm, DownLoadForm
 from apps.colorin.models import UploadedPhoto
 
 from apps.colorin.parsing.info import get_info
@@ -14,9 +14,32 @@ from apps.colorin.models import InstagramPhoto, InstagramProfile
 User = get_user_model()
 
 
-class IndexView(TemplateView):
+class IndexView(MultiFormsView):
     template_name = "colorin/index.html"
-    
+    form_classes = {'update_info': UpdateInfoForm,
+                    'update_photo': UpdateIGPhotoForm,
+                    'download': DownLoadForm}
+    success_url = '/colorin/'
+
+    #def get_login_initial(self):
+        #return {'email': 'dave@dave.com'}
+
+    #def get_signup_initial(self):
+        #return {'email': 'dave@dave.com'}
+
+    def get_context_data(self, **kwargs):
+        context = super(SignupLoginView, self).get_context_data(**kwargs)
+        context.update({"some_context_value": 'blah blah blah',
+                        "some_other_context_value": 'blah'})
+        return context
+
+    def update_info_form_valid(self, form):
+        return form.login(self.request, redirect_url=self.get_success_url())
+
+    def update_photo_form_valid(self, form):
+        user = form.save(self.request)
+        return form.signup(self.request, user, self.get_success_url())
+
     #def get(self, request, *args, **kwargs):
     #    get_info(request)
     #    return HttpResponse()
