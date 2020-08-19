5f0b4978c9b6b4c6f57f48f7bbf87bb6c8603fdc
ksradau <sushei.ekaterina@gmail.com>
2020-08-19 17:55:44 +0300

    Try to find num of matches by counter in common tolerance array

--- src/apps/colorin/palette/algorithm.py
+++ src/apps/colorin/palette/algorithm.py
@@ -17,14 +17,20 @@ def is_similar(rgb1, rgb2):
 
 
 def get_similar_array(color):
-    T = 20 + 1
+    T = 20
     tolerance_list = []
     r, g, b = color[0], color[1], color[2]
     for rn in range(r - T, r + T):
         for gn in range(g - T, g + T):
             for bn in range(b - T, b + T):
-                tolerance_list.append((rn, gn, bn,))
-    return tolerance_list
+                if rn > 255:
+                    rn = 255
+                if gn > 255:
+                    gn = 255
+                if bn > 255:
+                    bn = 255
+                tolerance_list.append((abs(rn), abs(gn), abs(bn),))
+    return set(tolerance_list)
 
 
 test_list_of_inst_colors = get_palette('examples\ex1.jpg', 6)
@@ -32,8 +38,23 @@ test_list_of_inst_colors = get_palette('examples\ex1.jpg', 6)
 palette_first_test_uploaded = get_palette('examples\ex2.jpg', 6)
 palette_second_test_uploaded = get_palette('examples\ex3.jpg', 6)
 
-test_uploaded_list = palette_first_test_uploaded + palette_second_test_uploaded
+test_uploaded_list = [palette_first_test_uploaded, palette_second_test_uploaded]
 
-similar_array_for_inst_palette = get_similar_array((150, 40, 90))
+_all = []
+for color in test_list_of_inst_colors:
+    similar_array_for_inst_palette = get_similar_array(color)
+    _all.append(similar_array_for_inst_palette)
 
-print(similar_array_for_inst_palette)
+print(_all)
+
+match_counter = 0
+for img in test_uploaded_list:
+    for palette in img:
+        for color in palette:
+            if color in _all:
+                match_counter += 1
+                print("~~~ It's a MATCH !!!")
+            else:
+                print("~~~ Oh no")
+    print("~~~ Num of matches:")
+    print(match_counter)
