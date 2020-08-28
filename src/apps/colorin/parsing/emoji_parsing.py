07bf7881b207ee97a792948889447e23f5c29078
ksradau <sushei.ekaterina@gmail.com>
2020-08-28 13:08:40 +0300

    Function for addition pics info to DB

--- src/apps/colorin/parsing/emoji_parsing.py
+++ src/apps/colorin/parsing/emoji_parsing.py
@@ -20,32 +20,25 @@ def get_similar_of_one_color(color):
                 if bn > 255:
                     bn = 255
                 tolerance_list.append((abs(rn), abs(gn), abs(bn),))
-    return set(tolerance_list)
-
-
-def parse_emoji():
-    with open("output.txt", "w") as txt:
-        for emoji_name in emoji_dict.keys():
-            url = "https://vk.com/images/emoji/" + emoji_name + ".png"
-            request = requests.get(url,
-                                   headers={'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"},
-                                   stream=True)
-            sleep(random.randint(1, 9))
-            lf = tempfile.NamedTemporaryFile(delete=False, suffix='.png')
-            lf.write(request.content)
-            palette = get_palette(lf, 2)
-            similar_array = get_similar_of_one_color(palette[0])
-            emoji_dict[emoji_name] = similar_array
-
-            txt.write(str(emoji_name) + '\n')
-            txt.write(str(similar_array) + '\n')
-
-            print(similar_array)
-
-        txt.write(emoji_dict)
-
-    print(emoji_dict)
+    return list(set(tolerance_list))
 
 
 def add_emoji(request):
-    pass
+    for emoji_name in emoji_dict.keys():
+        url = "https://vk.com/images/emoji/" + emoji_name + ".png"
+        request = requests.get(url,
+                               headers={'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"},
+                               stream=True)
+        sleep(random.randint(1, 9))
+        lf = tempfile.NamedTemporaryFile(delete=False, suffix='.png')
+        lf.write(request.content)
+        palette = get_palette(lf, 2)
+        dominant = palette[0]
+        similar = get_similar_of_one_color(dominant)
+
+        emoji = EmojiPic(emoji_name=emoji_name,
+                               dominant=dominant,
+                               similar=similar,
+                               )
+
+        emoji.save()
